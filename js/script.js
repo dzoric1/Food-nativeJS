4d6d5977511340fdfeec84cc97a775a249f79e46
dzoric1 <Gerasimov2099@mail.ru>
2023-01-30 21:19:15 +0800

    fix modal

--- js/script.js
+++ js/script.js
@@ -110,11 +110,14 @@ document.addEventListener('DOMContentLoaded', () => {
     modalCloseBtn = document.querySelector('[data-close]');
 
   modalTrigger.forEach(btn => {
-    btn.addEventListener('click', () => {
-      modal.classList.toggle('show');
-      document.body.style.overflow = 'hidden';
-    });
+    btn.addEventListener('click', openModal);
   });
+
+  function openModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
 
   function closeModal() {
     modal.classList.toggle('show');
@@ -134,4 +137,15 @@ document.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 15000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
